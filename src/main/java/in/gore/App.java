45bfdfe6b88440b19d137e265f88ff2b40b67111
Mihir Gore <mihirg@gmail.com>
2019-05-05 14:15:11 +0530

    Fix formatting

--- src/main/java/in/gore/App.java
+++ src/main/java/in/gore/App.java
@@ -17,35 +17,35 @@ import javax.management.ObjectName;
 
 
 public class App extends Application<TestConfiguration> {
-	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-	@Override
-	public void initialize(Bootstrap<TestConfiguration> b) {
+    @Override
+    public void initialize(Bootstrap<TestConfiguration> b) {
 
-	}
+    }
 
-	@Override
-	public void run(TestConfiguration c, Environment e) throws Exception {
-		LOGGER.info("Method App#run called.");
-		e.jersey().register(new QuoteResource());
-		e.jersey().register(new DetailsResource());
+    @Override
+    public void run(TestConfiguration c, Environment e) throws Exception {
+        LOGGER.info("Method App#run called.");
+        e.jersey().register(new QuoteResource());
+        e.jersey().register(new DetailsResource());
 
-		// register MBean with jmx server.
+        // register MBean with jmx server.
         // refer to http://www.oracle.com/us/technologies/java/best-practices-jsp-136021.html
         // for details on naming conventions.
-		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-		mbs.registerMBean(new CacheCounterMBeanImpl(), new ObjectName("in.gore:type=CacheCounter,name=Test1"));
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        mbs.registerMBean(new CacheCounterMBeanImpl(), new ObjectName("in.gore:type=CacheCounter,name=Test1"));
         mbs.registerMBean(new CacheCounterMBeanImpl(), new ObjectName("in.gore:type=CacheCounter,name=Test2"));
         mbs.registerMBean(new SingletonMBeanImpl(), new ObjectName("in.gore:type=ClassesLoaded"));
         LOGGER.info("MBeans registered");
 
-	}
+    }
 
-	public static void main(String args[]) {
-		try {
-			new App().run(args);
-		} catch (Exception exp) {
+    public static void main(String args[]) {
+        try {
+            new App().run(args);
+        } catch (Exception exp) {
             exp.printStackTrace();
-		}
-	}
+        }
+    }
 }
